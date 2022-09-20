1c2b7b3538892893f1d6f15cbe602cb8264e63ef
Francis Matias <fwmatias64@gmail.com>
2022-09-20 11:14:07 +0100

    Adding functions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", () => {
-    alert("Para com tudo")
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (element) => {
+    element.addEventListener("click",(event) => {
+        manipulaDados(event.target.textContent,event.target.parentNode)
+    })
 })
+function manipulaDados(operacao) {
+
+    const peca = document.querySelector(".controle-contador")
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
+    }else{
+        peca.value = parseInt(peca.value) + 1
+    }
+}
